1d8b058c4106bff9f66ac1dcc0c7aaee3cac107a
kuskas112 <vasileniyaivanka@mail.ru>
2024-11-19 15:55:04 +0300

    dont ask me

--- Алгоритмы и структуры данных/lab6/lab6Algs.cpp
+++ Алгоритмы и структуры данных/lab6/lab6Algs.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int r[6];
 string* keys;
 int* hashMap;
-int mapSize = 997;
+int mapSize = 1000;
 int keysAmount = 400;
 int iterationLimit = 100;
 
@@ -56,63 +56,191 @@ int multiplyMethod(int M, int k) {
 
 int linearMethod(string key, int (*hashMethod)(int, int), int hashParam, int c) {
     int k = xorMethod(key, r);
-    int interationCntr = 0;
-    while (interationCntr < iterationLimit)
+    int iterationCntr = 0;
+    while (iterationCntr < iterationLimit)
     {
-        int address = (hashMethod(hashParam, k) + (c * interationCntr++)) % mapSize;
+        int address = (hashMethod(hashParam, k) + (c * iterationCntr++)) % mapSize;
         if (hashMap[address] == 0) {
             hashMap[address]++;
-            return interationCntr;
-        }
-        else {
-            int kek = 0;
+            return iterationCntr;
         }
     }
     return -1;
 }
 
+int squareMethod(string key, int (*hashMethod)(int, int), int hashParam, int c, int d) {
+    int k = xorMethod(key, r);
+    int iterationCntr = 0;
+    while (iterationCntr < iterationLimit)
+    {
+        int address = (hashMethod(hashParam, k) + (c * iterationCntr) + (d * iterationCntr * iterationCntr)) % mapSize;
+        iterationCntr++;
+        if (hashMap[address] == 0) {
+            hashMap[address]++;
+            return iterationCntr;
+        }
+    }
+    return -1;
+}
 
-
-int main() {
-    setlocale(LC_ALL, "rus");
-    srand(static_cast<unsigned int>(std::time(0))); // Инициализация генератора случайных чисел
-    keys = new string[keysAmount];
-    hashMap = new int[mapSize];
-    getR(r, 6);
-    clearHashMap(hashMap, mapSize);
-
+double averageLD() {
     double sum = 0;
     for (int i = 0; i < keysAmount; i++)
     {
-        keys[i] = getKey();
         sum += linearMethod(keys[i], divideHashMethod, mapSize / 3, 139);
     }
-    cout << "Средняя длина пути линейного метода + метода деления " << sum / keysAmount << endl;
-    
     clearHashMap(hashMap, mapSize);
+    return sum / keysAmount;
+}
 
-    sum = 0;
+double averageLM() {
+    double sum = 0;
     for (int i = 0; i < keysAmount; i++)
     {
         sum += linearMethod(keys[i], multiplyMethod, mapSize, 139);
     }
-    cout << "Средняя длина пути линейного метода + метода умножения " << sum / keysAmount << endl;
+    clearHashMap(hashMap, mapSize);
+    return sum / keysAmount;
+}
 
-    ofstream outputFile("data.csv");
+double averageSD() {
+    double sum = 0;
+    for (int i = 0; i < keysAmount; i++)
+    {
+        sum += squareMethod(keys[i], divideHashMethod, mapSize / 3, 577, 14);
+    }
+    clearHashMap(hashMap, mapSize);
+    return sum / keysAmount;
+}
 
+double averageSM() {
+    double sum = 0;
+    for (int i = 0; i < keysAmount; i++)
+    {
+        sum += squareMethod(keys[i], multiplyMethod, mapSize, 577, 14);
+    }
+    clearHashMap(hashMap, mapSize);
+    return sum / keysAmount;
+}
+
+void generateKeys() {
+    for (int i = 0; i < keysAmount; i++)
+    {
+        keys[i] = getKey();
+    }
+}
+
+void generate() {
+    keys = new string[keysAmount];
+    hashMap = new int[mapSize];
+    getR(r, 6);
+    generateKeys();
+    clearHashMap(hashMap, mapSize);
+    cout << "Средняя длина пути линейного метода + метода деления " << averageLD() << endl;
+    cout << "Средняя длина пути линейного метода + метода умножения " << averageLM() << endl;
+    cout << "Средняя длина пути квадратичного метода + метода деления " << averageSD() << endl;
+    cout << "Средняя длина пути квадратичного метода + метода умножения " << averageSM() << endl;
+    delete[] keys;
+    delete[] hashMap;
+}
+
+void createAvg(string fileName, double(*averageFunc)()) {
+    keys = new string[mapSize];
+    hashMap = new int[mapSize];
+    clearHashMap(hashMap, mapSize);
+    ofstream outputFile(fileName);
     if (!outputFile) {
         std::cerr << "Не удалось открыть файл!" << std::endl;
-        return 1;
+        return;
     }
 
-    for (int i = 0; i < mapSize; ++i) {
-        outputFile << hashMap[i];
-        if (i < mapSize - 1) { // Если это не последний элемент, добавляем запятую
-            outputFile << ";";
+    for (keysAmount = 10; keysAmount < mapSize - 10; keysAmount += 10)
+    {
+        for (int i = 0; i < keysAmount; i++)
+        {
+            keys[i] = getKey();
+        }
+        double avg = averageFunc();
+        if (avg == -1) { continue; }
+        cout << "AVG: " << avg << " keysAmount: " << keysAmount << endl;
+        outputFile << keysAmount << ";" << avg << endl;
+    }
+    outputFile.close();
+    delete[] keys;
+    delete[] hashMap;
+}
+
+int getValidNumber() {
+    int number;
+    while (true) {
+        cin >> number;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Ошибка: введите корректное целое число!" << endl;
+        }
+        else {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            break;
         }
     }
-    outputFile << endl;
-    clearHashMap(hashMap, mapSize);
+    return number;
+}
 
-    return 0;
+
+int main() {
+    srand(static_cast<unsigned int>(std::time(0))); // Инициализация генератора случайных чисел
+    int temp;
+    // Устанавливаем локаль для вывода с запятой вместо точки
+    std::locale::global(std::locale("ru_RU.UTF-8"));
+    std::cout.imbue(std::locale());
+    setlocale(LC_ALL, "RUS");
+    createAvg("LD.csv", averageLD);
+    createAvg("LM.csv", averageLM);
+    createAvg("SD.csv", averageSD);
+    createAvg("SM.csv", averageSM);
+    /*while (true)
+    {
+        cout << "Размер таблицы: " << mapSize;
+        cout << " Количество ключей: " << keysAmount << endl;
+        cout << "1. Ввести размер хеш таблицы" << endl;
+        cout << "2. Ввести количество ключей" << endl;
+        cout << "3. Смоделировать хеширование" << endl;
+        cout << "0. Выход" << endl;
+        char c;
+        cin >> c;
+        switch (c)
+        {
+        case '1':
+            cout << "Введите размер" << endl;
+            temp = getValidNumber();
+            if (keysAmount > temp) {
+                cout << "Невалидный размер хеш таблицы" << endl;
+                break;
+            }
+            mapSize = temp;
+            break;
+        case '2':
+            cout << "Введите число ключей" << endl;
+            temp = getValidNumber();
+            if (mapSize < temp) {
+                cout << "Невалидное количество ключей" << endl;
+                break;
+            }
+            keysAmount = temp;
+            break;
+        case '3':
+            generate();
+            break;
+        case '0':
+            return 0;
+
+        default:
+            cout << "Неизвестная команда\n";
+            break;
+        }
+
+
+    }*/
 }
