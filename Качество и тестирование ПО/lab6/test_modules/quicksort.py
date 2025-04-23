14c6ffd5a8302b88fab408189daf6cb557c38a85
kuskas112 <vasileniyaivanka@mail.ru>
2025-04-23 20:23:06 +0300

    отчеты тестирование

--- Качество и тестирование ПО/lab6/test_modules/quicksort.py
+++ Качество и тестирование ПО/lab6/test_modules/quicksort.py
@@ -2,7 +2,6 @@ def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-        # Перемещаем элементы, которые больше ключа, на одну позицию вперед
         while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
             j -= 1
